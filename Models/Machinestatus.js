4d7523f91a8f81083911ed57edcb6084bf8cf9f3
Kartheeshwaran .G <122508393+Kartheeshmca@users.noreply.github.com>
2025-12-03 21:00:37 +0530

    Delete Models directory

--- Models/Machinestatus.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const machineStatusSchema = new mongoose.Schema({
-  machineNo: { type: String, required: true, unique: true },
-  lastStatus: { type: String, required: true },
-  updatedAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("MachineStatus", machineStatusSchema);
